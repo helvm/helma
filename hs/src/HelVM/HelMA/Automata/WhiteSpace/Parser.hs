92c743e4edc3087354047dcecac1e0e72f03bf24
Kamil Adam <kamil.adam.zabinski@gmail.com>
2022-09-15 20:31:55 +0200

    Refactor parseFromTL

--- hs/src/HelVM/HelMA/Automata/WhiteSpace/Parser.hs
+++ hs/src/HelVM/HelMA/Automata/WhiteSpace/Parser.hs
@@ -43,58 +43,71 @@ parse tokenType = flip parseFromTL . tokenize tokenType
 parseFromTL :: MonadSafe m => Bool -> TokenList -> m InstructionList
 parseFromTL ascii = go where
   go :: MonadSafe m => TokenList -> m InstructionList
-  go []            = pure []
-  --Stack Manipulation instructions
-  go (S:S:tl')     = go' =<< parseSymbol tl' where go' (symbol , tl'') = (IAL (Cons symbol)  : ) <$> go tl''
-  go (S:T:S:tl')   = go' =<< parseIndex  tl' where go' (index  , tl'') = (IAL (SStatic index Copy)     : ) <$> go tl''
-  go (S:T:T:tl')   = panic "STT" tl'
-  go (S:T:N:tl')   = go' =<< parseIndex  tl' where go' (index  , tl'') = (IAL (SStatic index Slide)    : ) <$> go tl''
-  go (S:N:S:tl')   = (IAL Dup               : ) <$> go tl'
-  go (S:N:T:tl')   = (IAL Swap              : ) <$> go tl'
-  go (S:N:N:tl')   = (IAL Discard           : ) <$> go tl'
-  --Arithmetic
-  go (T:S:S:S:tl') = (IAL (Binary Add)      : ) <$> go tl'
-  go (T:S:S:T:tl') = (IAL (Binary Sub)      : ) <$> go tl'
-  go (T:S:S:N:tl') = (IAL (Binary Mul)      : ) <$> go tl'
-  go (T:S:T:S:tl') = (IAL (Binary Div)      : ) <$> go tl'
-  go (T:S:T:T:tl') = (IAL (Binary Mod)      : ) <$> go tl'
-  go (T:S:T:N:tl') = panic "TSTN" tl'
-  go (T:S:N:S:tl') = panic "TSNS" tl'
-  go (T:S:N:T:tl') = panic "TSNT" tl'
-  go (T:S:N:N:tl') = panic "TSNN" tl'
-  --Heap access
-  go (T:T:S:tl')   = (ILS Store            : ) <$> go tl'
-  go (T:T:T:tl')   = (ILS Load             : ) <$> go tl'
-  go (T:T:N:tl')   = panic "TTN" tl'
-  --IControl
-  go (N:S:S:tl')   = go' =<< parseLabel ascii tl' where go' (label  , tl'') = (IControl (Mark    label) : ) <$> go tl''
-  go (N:S:T:tl')   = go' =<< parseLabel ascii tl' where go' (label  , tl'') = (IControl (CStatic label Call       ) : ) <$> go tl''
-  go (N:S:N:tl')   = go' =<< parseLabel ascii tl' where go' (label  , tl'') = (IControl (CStatic label Jump       ) : ) <$> go tl''
-  go (N:T:S:tl')   = go' =<< parseLabel ascii tl' where go' (label  , tl'') = (IControl (CStatic label (Branch EZ)) : ) <$> go tl''
-  go (N:T:T:tl')   = go' =<< parseLabel ascii tl' where go' (label  , tl'') = (IControl (CStatic label (Branch LTZ)) : ) <$> go tl''
-  go (N:T:N:tl')   = (IControl Return           : ) <$> go tl'
-  go (N:N:S:tl')   = panic "NNS" tl'
-  go (N:N:T:tl')   = panic "NNT" tl'
-  go (N:N:N:tl')   = (End                            : ) <$> go tl'
-  --IO instructions
-  go (T:N:S:S:tl') = (IAL (SIO OutputChar)        : ) <$> go tl'
-  go (T:N:S:T:tl') = (IAL (SIO OutputDec)         : ) <$> go tl'
-  go (T:N:S:N:tl') = panic "TNSN" tl'
-  go (T:N:T:S:tl') = (ILS (MIO InputChar)         : ) <$> go tl'
-  go (T:N:T:T:tl') = (ILS (MIO InputDec)          : ) <$> go tl'
-  go (T:N:T:N:tl') = panic "TNTN" tl'
-  go (T:N:N:S:tl') = panic "TNNS" tl'
-  go (T:N:N:T:tl') = panic "TNNT" tl'
-  go (T:N:N:N:tl') = panic "TNNN" tl'
-  go tl'           = panic (show tl') []
---
---  stackManipulation (S:S:tl')     = go' =<< parseSymbol tl' where go' (symbol , tl'') = (IAL (Cons symbol)  : ) <$> go tl''
---  stackManipulation (S:T:S:tl')   = go' =<< parseIndex  tl' where go' (index  , tl'') = (IAL (SStatic index Copy)     : ) <$> go tl''
---  stackManipulation (S:T:T:tl')   = panic "STT" tl'
---  stackManipulation (S:T:N:tl')   = go' =<< parseIndex  tl' where go' (index  , tl'') = (IAL (SStatic index Slide)    : ) <$> go tl''
---  stackManipulation (S:N:S:tl')   = (IAL Dup               : ) <$> go tl'
---  stackManipulation (S:N:T:tl')   = (IAL Swap              : ) <$> go tl'
---  stackManipulation (S:N:N:tl')   = (IAL Discard           : ) <$> go tl'
+  go      []   = pure              []
+  go (S:  tl') = stackManipulation tl'
+  go (T:S:tl') = arithmetic        tl'
+  go (T:T:tl') = heapAccess        tl'
+  go (N  :tl') = flowControl       tl'
+  go (T:N:tl') = ioInstruction     tl'
+  go      tl'  = notEnoughTokens   tl'
 
-panic :: MonadSafe m => Text -> TokenList -> m InstructionList
-panic token tl = liftErrorTupleList [("Unrecognised tokenl" , token) , ("Rest tokens" , show tl)]
+  stackManipulation :: MonadSafe m => TokenList -> m InstructionList
+  stackManipulation (S:  tl') = go' =<< parseSymbol tl' where go' (symbol , tl'') = (IAL (Cons symbol)         : ) <$> go tl''
+  stackManipulation (T:S:tl') = go' =<< parseIndex  tl' where go' (index  , tl'') = (IAL (SStatic index Copy)  : ) <$> go tl''
+  stackManipulation (T:T:tl') = unrecognisedTokens "STT" tl'
+  stackManipulation (T:N:tl') = go' =<< parseIndex  tl' where go' (index  , tl'') = (IAL (SStatic index Slide) : ) <$> go tl''
+  stackManipulation (N:S:tl') = (IAL Dup     : ) <$> go tl'
+  stackManipulation (N:T:tl') = (IAL Swap    : ) <$> go tl'
+  stackManipulation (N:N:tl') = (IAL Discard : ) <$> go tl'
+  stackManipulation      tl'  = notEnoughTokensForIMP "stackManipulation" tl'
+
+  arithmetic :: MonadSafe m => TokenList -> m InstructionList
+  arithmetic (S:S:tl') = (IAL (Binary Add) : ) <$> go tl'
+  arithmetic (S:T:tl') = (IAL (Binary Sub) : ) <$> go tl'
+  arithmetic (S:N:tl') = (IAL (Binary Mul) : ) <$> go tl'
+  arithmetic (T:S:tl') = (IAL (Binary Div) : ) <$> go tl'
+  arithmetic (T:T:tl') = (IAL (Binary Mod) : ) <$> go tl'
+  arithmetic (T:N:tl') = unrecognisedTokens "TSTN" tl'
+  arithmetic (N:S:tl') = unrecognisedTokens "TSNS" tl'
+  arithmetic (N:T:tl') = unrecognisedTokens "TSNT" tl'
+  arithmetic (N:N:tl') = unrecognisedTokens "TSNN" tl'
+  arithmetic      tl'  = notEnoughTokensForIMP "arithmetic" tl'
+
+  heapAccess :: MonadSafe m => TokenList -> m InstructionList
+  heapAccess (S:tl') = (ILS Store : ) <$> go tl'
+  heapAccess (T:tl') = (ILS Load  : ) <$> go tl'
+  heapAccess (N:tl') = unrecognisedTokens "TTN" tl'
+  heapAccess    tl'  = notEnoughTokensForIMP "heapAccess" tl'
+
+  flowControl :: MonadSafe m => TokenList -> m InstructionList
+  flowControl (S:S:tl') = go' =<< parseLabel ascii tl' where go' (label , tl'') = (IControl (Mark    label             ) : ) <$> go tl''
+  flowControl (S:T:tl') = go' =<< parseLabel ascii tl' where go' (label , tl'') = (IControl (CStatic label Call        ) : ) <$> go tl''
+  flowControl (S:N:tl') = go' =<< parseLabel ascii tl' where go' (label , tl'') = (IControl (CStatic label Jump        ) : ) <$> go tl''
+  flowControl (T:S:tl') = go' =<< parseLabel ascii tl' where go' (label , tl'') = (IControl (CStatic label (Branch EZ )) : ) <$> go tl''
+  flowControl (T:T:tl') = go' =<< parseLabel ascii tl' where go' (label , tl'') = (IControl (CStatic label (Branch LTZ)) : ) <$> go tl''
+  flowControl (T:N:tl') = (IControl Return : ) <$> go tl'
+  flowControl (N:S:tl') = unrecognisedTokens "NNS" tl'
+  flowControl (N:T:tl') = unrecognisedTokens "NNT" tl'
+  flowControl (N:N:tl') = (End             : ) <$> go tl'
+  flowControl      tl'  = notEnoughTokensForIMP "flowControl" tl'
+
+  ioInstruction :: MonadSafe m => TokenList -> m InstructionList
+  ioInstruction (S:S:tl') = (IAL (SIO OutputChar) : ) <$> go tl'
+  ioInstruction (S:T:tl') = (IAL (SIO OutputDec)  : ) <$> go tl'
+  ioInstruction (S:N:tl') = unrecognisedTokens "TNSN" tl'
+  ioInstruction (T:S:tl') = (ILS (MIO InputChar)  : ) <$> go tl'
+  ioInstruction (T:T:tl') = (ILS (MIO InputDec)   : ) <$> go tl'
+  ioInstruction (T:N:tl') = unrecognisedTokens "TNTN" tl'
+  ioInstruction (N:S:tl') = unrecognisedTokens "TNNS" tl'
+  ioInstruction (N:T:tl') = unrecognisedTokens "TNNT" tl'
+  ioInstruction (N:N:tl') = unrecognisedTokens "TNNN" tl'
+  ioInstruction tl'       = notEnoughTokensForIMP "ioInstruction" tl'
+
+unrecognisedTokens :: MonadSafe m => Text -> TokenList -> m InstructionList
+unrecognisedTokens tokens tl = liftErrorTupleList [("Unrecognised tokens" , tokens) , ("Rest tokens" , show tl)]
+
+notEnoughTokensForIMP :: MonadSafe m => Text -> TokenList -> m InstructionList
+notEnoughTokensForIMP imp tl = liftErrorTupleList [("Not enough tokens for IMP:" , imp) , ("Rest tokens" , show tl)]
+
+notEnoughTokens :: MonadSafe m => TokenList -> m InstructionList
+notEnoughTokens tl = liftErrorTupleList [("Rest tokens" , show tl)]
