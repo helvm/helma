0328334e91fcb74035c2398daee058716866c8c4
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-01-18 18:45:55 +0100

    Refactor Automaton

--- hs/src/HelVM/HelMA/Automata/BrainFuck/Impl/Tree/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/BrainFuck/Impl/Tree/Evaluator.hs
@@ -1,22 +1,26 @@
 module HelVM.HelMA.Automata.BrainFuck.Impl.Tree.Evaluator (
   evalSource,
+  currentSymbolSafe,
 ) where
 
 import           HelVM.HelMA.Automata.BrainFuck.Impl.Tree.Instruction
 import           HelVM.HelMA.Automata.BrainFuck.Impl.Tree.Parser
 
+import           HelVM.HelMA.Automata.BrainFuck.Impl.Tree.InstructionUnit (InstructionUnit (IU))
+import qualified HelVM.HelMA.Automata.BrainFuck.Impl.Tree.InstructionUnit as IU
+
 import           HelVM.HelMA.Automata.BrainFuck.Common.PureInstruction
 import           HelVM.HelMA.Automata.BrainFuck.Common.SimpleInstruction
 import           HelVM.HelMA.Automata.BrainFuck.Common.Symbol
 import           HelVM.HelMA.Automata.BrainFuck.Common.TapeOfSymbols
-import qualified HelVM.HelMA.Automata.BrainFuck.Common.TapeOfSymbols     as Tape
+import qualified HelVM.HelMA.Automata.BrainFuck.Common.TapeOfSymbols      as Tape
 
 import           HelVM.HelMA.Automaton.API.IOTypes
 import           HelVM.HelMA.Automaton.IO.BusinessIO
 import           HelVM.HelMA.Automaton.Loop
 import           HelVM.HelMA.Automaton.Types.DumpType
 
-import           HelVM.HelIO.Containers.LLIndexSafe
+import           HelVM.HelIO.Control.Safe
 
 import           Control.Type.Operator
 
@@ -27,7 +31,10 @@ runVector :: (BIO m , Symbol e) => TreeInstructionVector -> FullTape e -> m $ Au
 runVector iv = nextStep (IU iv 0)
 
 nextStep :: (BIO m , Symbol e) => InstructionUnit -> FullTape e -> m $ Automaton e
-nextStep  (IU iv ic) tape = doInstructionOpt (iv `indexMaybe` ic) (Automaton (IU iv $ ic + 1) tape)
+nextStep iu tape = nextStepA (Automaton iu tape)
+
+nextStepA :: (BIO m , Symbol e) => Automaton e -> m $ Automaton e
+nextStepA a = doInstructionOpt (currentInstruction a) (nextIC a)
 
 doInstructionOpt :: (BIO m , Symbol e) => Maybe TreeInstruction -> Automaton e -> m $ Automaton e
 doInstructionOpt (Just i) = doInstruction i
@@ -62,10 +69,23 @@ doPure MoveL = Tape.moveHeadLeft
 doPure Inc   = Tape.nextSymbol
 doPure Dec   = Tape.prevSymbol
 
+-- | Accessors
+
+currentSymbolSafe :: MonadSafe m => Automaton e -> m e
+currentSymbolSafe = Tape.readSymbolSafe . unitTape
+
+currentInstruction :: Automaton e -> Maybe TreeInstruction
+currentInstruction = IU.currentInstruction . unitUI
+
 -- | Constructors
 
---updateUI :: (InstructionUnit -> InstructionUnit) -> Automaton e -> Automaton e
---updateUI f a = a { unitUI = f $ unitUI a }
+nextIC :: Automaton e -> Automaton e
+nextIC = updateUI IU.nextIC
+
+-- |
+
+updateUI :: (InstructionUnit -> InstructionUnit) -> Automaton e -> Automaton e
+updateUI f a = a { unitUI = f $ unitUI a }
 
 --updateTape :: (FullTape e -> FullTape e) -> Automaton e -> Automaton e
 --updateTape f a = a { unitTape = f $ unitTape a }
@@ -78,8 +98,3 @@ data Automaton e = Automaton
   , unitTape :: FullTape e
   }
   deriving stock (Eq , Show)
-
-data InstructionUnit = IU !TreeInstructionVector !InstructionCounter
-  deriving stock (Eq , Show)
-
-type InstructionCounter = Int
