b4874f51ccace97fb232737179b046cece5c5f32
Kamil Adam <kamil.adam.zabinski@gmail.com>
2023-07-25 23:26:54 +0200

    WIP

--- hs/src/HelVM/HelMA/Automata/Piet/LabellingStatus.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LabellingStatus.hs
@@ -5,35 +5,38 @@ import           HelVM.HelMA.Automata.Piet.EquivalenceMap
 import           HelVM.HelMA.Automata.Piet.FullInfo
 import           HelVM.HelMA.Automata.Piet.Image
 
-import qualified Relude.Extra                             as Extra
-
+import           Data.Function.Tools
 import           Data.IntMap                              (adjust, insert)
 
+import qualified Relude.Extra                             as Extra
+
 updateStatus :: [LabelKey]  -> Coordinates -> LabellingStatus -> LabellingStatus
-updateStatus [] (x , y) status = status
+updateStatus []       = updateStatus0
+updateStatus [label]  = updateStatus1 label
+updateStatus [l1, l2] = updateStatus2 l1 l2
+updateStatus _        = const2 (error "too many neighbours in updateStatus")
+
+updateStatus0 :: Coordinates -> LabellingStatus -> LabellingStatus
+updateStatus0 (x , y) status = status
   { nextKey  = Extra.next label
-  , mask    = imgSetPixel (x , y) label (mask status)
+  , mask     = imgSetPixel (x , y) label (mask status)
   , infoMap  = insert label (addPixel (x , y) mempty) (infoMap status)
   } where
     label = nextKey status
-updateStatus [label] (x , y) status = status
-  { mask    = imgSetPixel (x , y) label (mask status)
+
+updateStatus1 :: LabelKey -> Coordinates -> LabellingStatus -> LabellingStatus
+updateStatus1 label (x , y) status = status
+  { mask     = imgSetPixel (x , y) label (mask status)
   , infoMap  = adjust (addPixel (x , y)) label (infoMap status)
   }
-updateStatus [l1, l2] (x , y) status = status
-  { mask    = imgSetPixel (x , y) label (mask status)
-  , infoMap  = adjust (addPixel (x, y)) label (infoMap status)
+
+updateStatus2 :: LabelKey -> LabelKey -> Coordinates -> LabellingStatus -> LabellingStatus
+updateStatus2 l1 l2 (x , y) status = status
+  { mask          = imgSetPixel (x , y) label (mask status)
+  , infoMap       = adjust (addPixel (x, y)) label (infoMap status)
   , equivalences  = equivInsert l1 l2 (equivalences status)
   } where
     label = max l1 l2
-updateStatus _ _ _ = error "too many neighbours in updateStatus"
-
---updateStatus0 (x , y) status
---  { nextKey  = Extra.next label
---  , mask    = imgSetPixel (x , y) label (mask status)
---  , infoMap  = insert label (addPixel (x , y) mempty) (infoMap status)
---  } where
---    label = nextKey status
 
 newLabellingStatus :: Image a -> LabellingStatus
 newLabellingStatus img = LabellingStatus
