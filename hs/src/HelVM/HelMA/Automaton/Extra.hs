70495662195ebc496febf1686d9488870d6e629f
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-07-23 22:55:28 +0200

    Remove logger

--- hs/src/HelVM/HelMA/Automaton/Extra.hs
+++ hs/src/HelVM/HelMA/Automaton/Extra.hs
@@ -5,19 +5,16 @@ import           HelVM.HelMA.Automaton.API.Env
 import           HelVM.HelMA.Automaton.API.EvalParams
 import           HelVM.HelMA.Automaton.API.IOTypes
 
-import           HelVM.HelIO.Control.Control
+import           HelVM.HelIO.Control.Safe
 import           HelVM.HelIO.Control.Message
-
-import           Control.Monad.Writer.Lazy            (runWriterT)
 
 import qualified Data.DList                           as D
 
 import qualified RIO
 
-runAsRIO :: (MonadIO m, MonadReader env m, Has env) => ControlT m a -> m a
+runAsRIO :: (MonadIO m, MonadReader env m, Has env) => SafeT m a -> m a
 runAsRIO rio = do
-  (result, logs) <- runWriterT $ runExceptT rio
-  forM_ (D.toList logs) (RIO.logInfo . RIO.display)
+  result <- runExceptT rio
   either ((*> RIO.exitFailure) . RIO.logError . RIO.display . errorsToText) pure result
 
 readSourceFile :: Has env => Exec -> String -> RIO.RIO env Source
