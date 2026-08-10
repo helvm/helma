c2bb11dcda6a4faee70a4ad7f1f9867556ebe09b
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-08-10 16:30:23 +0200

    Add GadtEffSpec

---
+++ hs/src/HelVM/HelMA/Automaton/Eff/GADTEff.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE GADTs      #-}
+{-# LANGUAGE RankNTypes #-}
+
+module HelVM.HelMA.Automaton.Eff.GADTEff (
+  interpretGADTEffDebug,
+  interpretGADTEff,
+  GADTEff,
+  GADTEffF(..),
+) where
+
+import           HelVM.HelMA.Automaton.Eff.MonadEff
+
+import           Control.Monad.Logger
+
+import           Prelude                            hiding (getLine, putLTextLn, putText, putTextLn)
+
+newtype GADTEff a = GADTEff { runGADTEff :: forall m. Monad m => (forall x. GADTEffF x -> m x) -> m a }
+
+instance Functor GADTEff where
+  fmap f m = GADTEff $ \k -> fmap f (runGADTEff m k)
+
+instance Applicative GADTEff where
+  pure a  = GADTEff $ \_ -> pure a
+  f <*> a = GADTEff $ \k -> runGADTEff f k <*> runGADTEff a k
+
+instance Monad GADTEff where
+  return = pure
+  m >>= f = GADTEff $ \k -> runGADTEff m k >>= \a -> runGADTEff (f a) k
+
+liftF :: GADTEffF a -> GADTEff a
+liftF fa = GADTEff $ \k -> k fa
+
+--------------------------------------------------------------------------------
+-- Interpretacja
+
+interpretGADTEffDebug :: MonadLoggerEff m => GADTEff a -> m a
+interpretGADTEffDebug eff = runGADTEff eff interpretGADTEffFDebug
+
+interpretGADTEff :: MonadEff m => GADTEff a -> m a
+interpretGADTEff eff = runGADTEff eff interpretGADTEffF
+
+--------------------------------------------------------------------------------
+-- Interpreter dla pojedynczych instrukcji (bez fmap/kontynuacji!)
+
+interpretGADTEffFDebug :: MonadLoggerEff m => GADTEffF a -> m a
+interpretGADTEffFDebug GetContentsBS   = logDebugN "GetContentsBS"   *> getContentsBS
+interpretGADTEffFDebug GetContentsText = logDebugN "GetContentsText" *> getContentsText
+interpretGADTEffFDebug GetChar         = logAndCont =<< getChar where logAndCont c = logDebugN ("GetChar: " <> one c) $> c
+interpretGADTEffFDebug GetLine         = logAndCont =<< getLine where logAndCont l = logDebugN ("GetLine: " <>     l) $> l
+interpretGADTEffFDebug (PutChar c)     = logDebugN ("PutChar: " <> one c) *> putChar c
+interpretGADTEffFDebug (PutText s)     = logDebugN ("PutText: " <>     s) *> putTextEff s
+interpretGADTEffFDebug Flush           = logDebugN "Flush"                *> flush
+
+interpretGADTEffF :: MonadEff m => GADTEffF a -> m a
+interpretGADTEffF GetContentsBS   = getContentsBS
+interpretGADTEffF GetContentsText = getContentsText
+interpretGADTEffF GetChar         = getChar
+interpretGADTEffF GetLine         = getLine
+interpretGADTEffF (PutChar c)     = putChar c
+interpretGADTEffF (PutText s)     = putTextEff s
+interpretGADTEffF Flush           = flush
+
+--------------------------------------------------------------------------------
+
+instance MonadEff GADTEff where
+  getContentsBS   = gadtGetContentsBS
+  getContentsText = gadtGetContentsText
+  getChar         = gadtGetChar
+  getLine         = gadtGetLine
+  putChar         = gadtPutChar
+  putTextEff      = gadtPutTextEff
+  flush           = gadtFlush
+
+gadtGetContentsBS :: GADTEff LByteString
+gadtGetContentsBS = liftF GetContentsBS
+
+gadtGetContentsText :: GADTEff LText
+gadtGetContentsText = liftF GetContentsText
+
+gadtGetChar :: GADTEff Char
+gadtGetChar = liftF GetChar
+
+gadtGetLine :: GADTEff Text
+gadtGetLine = liftF GetLine
+
+gadtPutChar :: Char -> GADTEff ()
+gadtPutChar = liftF . PutChar
+
+gadtPutTextEff :: Text -> GADTEff ()
+gadtPutTextEff = liftF . PutText
+
+gadtFlush :: GADTEff ()
+gadtFlush = liftF Flush
+
+--------------------------------------------------------------------------------
+
+data GADTEffF a where
+  GetContentsBS   :: GADTEffF LByteString
+  GetContentsText :: GADTEffF LText
+  GetChar         :: GADTEffF Char
+  GetLine         :: GADTEffF Text
+  PutChar         :: Char -> GADTEffF ()
+  PutText         :: Text -> GADTEffF ()
+  Flush           :: GADTEffF ()
---
+++ hs/test/HelVM/HelMA/Automaton/Eff/GADTEffSpec.hs
@@ -0,0 +1,54 @@
+module HelVM.HelMA.Automaton.Eff.GADTEffSpec (spec) where
+
+import           HelVM.HelMA.Automaton.Eff.GADTEff
+import           HelVM.HelMA.Automaton.Eff.Mock
+import           HelVM.HelMA.Automaton.Eff.MonadEff
+
+import           Test.Hspec                         (Spec, describe, it)
+import           Test.Hspec.Expectations.Pretty
+
+wFilterIf0 :: MonadEff m => m ()
+wFilterIf0 = do
+  char <- getChar
+  if char == '0'
+    then do
+      putChar '\n'
+    else do
+      putChar char
+      wFilterIf0
+
+spec :: Spec
+spec = do
+  describe "Test WFilter0" $ do
+    let mockEff = execMockEffWithInput "qwerty0uiop" wFilterIf0
+    forM_
+      [ ("Test WFilterIf0 with calculateOutput" , calculateOutput , "qwerty\n")
+      , ("Test WFilterIf0 with calculateLogsWithLevelDebug" , calculateLogsWithLevelDebug , ""      )
+      ] $ \(name , action , output) ->
+      it name $ action mockEff `shouldBe` output
+  describe "Test GADT WFilter0" $ do
+    let mockEff = execMockEffWithInput "qwerty0uiop" $ interpretGADTEffDebug wFilterIf0
+    forM_
+      [ ("Test GADT WFilterIf0 with calculateOutput" , calculateOutput , "qwerty\n"       )
+      , ("Test GADT WFilterIf0 with calculateLogsWithLevelDebug" , calculateLogsWithLevelDebug , debugLogs)
+      ] $ \(name , action , output) ->
+      it name $ action mockEff `shouldBe` output
+
+debugLogs :: Text
+debugLogs =
+  "Debug GetChar: q\n\
+  \Debug PutChar: q\n\
+  \Debug GetChar: w\n\
+  \Debug PutChar: w\n\
+  \Debug GetChar: e\n\
+  \Debug PutChar: e\n\
+  \Debug GetChar: r\n\
+  \Debug PutChar: r\n\
+  \Debug GetChar: t\n\
+  \Debug PutChar: t\n\
+  \Debug GetChar: y\n\
+  \Debug PutChar: y\n\
+  \Debug GetChar: 0\n\
+  \Debug PutChar: \n\
+  \\n"
+
