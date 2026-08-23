5df7ea677f318fc9a25f4ab015e2b2dff6da833d
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-08-23 18:56:52 +0200

    Reformat parser

--- hs/src/HelVM/HelMA/Automata/Piet/Parser.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/Parser.hs
@@ -33,32 +33,30 @@ calculateActualCodelLength codelInfo img = max 1 $ maybe defaultCodelInfo fromIn
   defaultCodelInfo = imageGuessCodelLength img
 
 imageGuessCodelLength ∷ Picture.Image Picture.PixelRGB8 → Int
-imageGuessCodelLength img = lastUntil isOne $ scanl gcd (gcd width height) $ fmap olength (group rows) <> fmap olength (group cols)
-  where
-    width  = Picture.imageWidth img
-    height = Picture.imageHeight img
-    isOne  = (== 1)
+imageGuessCodelLength img = lastUntil isOne $ scanl gcd (gcd width height) $ fmap olength (group rows) <> fmap olength (group cols) where
+  width  = Picture.imageWidth img
+  height = Picture.imageHeight img
+  isOne  = (== 1)
 
-    rows = [ [ Picture.pixelAt img x y | x <- [0 .. width-1] ]  | y <- [0 .. height-1] ]
-    cols = [ [ Picture.pixelAt img x y | y <- [0 .. height-1] ] | x <- [0 .. width-1] ]
+  rows = [ [ Picture.pixelAt img x y | x <- [0 .. width-1] ]  | y <- [0 .. height-1] ]
+  cols = [ [ Picture.pixelAt img x y | y <- [0 .. height-1] ] | x <- [0 .. width-1] ]
 
-    lastUntil _ [x]    = x
-    lastUntil p (x:xs) = guardPred (p x) p x xs
-    lastUntil _ _      = error "empty list in lastUntil helper (imageGuessCodelLength)"
+  lastUntil _ [x]    = x
+  lastUntil p (x:xs) = guardPred (p x) p x xs
+  lastUntil _ _      = error "empty list in lastUntil helper (imageGuessCodelLength)"
 
-    guardPred True  _  x _  = x
-    guardPred False p' _ xs = lastUntil p' xs
+  guardPred True  _  x _  = x
+  guardPred False p' _ xs = lastUntil p' xs
 
 imageToColorImage ∷ Int → Picture.Image Picture.PixelRGB8 → Image Color
-imageToColorImage cs img = newImage (w', h') assocList
-  where
-    w  = Picture.imageWidth img
-    h  = Picture.imageHeight img
-    w' = w `div` cs
-    h' = h `div` cs
+imageToColorImage cs img = newImage (w', h') assocList where
+  w  = Picture.imageWidth img
+  h  = Picture.imageHeight img
+  w' = w `div` cs
+  h' = h `div` cs
 
-    assocList =
-      [ ((x, y), pixelToColor (Picture.pixelAt img (x * cs) (y * cs)))
-      | y <- [0 .. h' - 1]
-      , x <- [0 .. w' - 1]
-      ]
+  assocList =
+    [ ((x, y), pixelToColor (Picture.pixelAt img (x * cs) (y * cs)))
+    | y <- [0 .. h' - 1]
+    , x <- [0 .. w' - 1]
+    ]
