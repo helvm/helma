11810bff460991bfcd2fe81868b683812229b961
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-08-27 19:01:37 +0200

    Add LLVM

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/Internal/Position.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/Internal/Position.hs
@@ -6,7 +6,7 @@ import           HelVM.HelMA.Automata.Piet.LLVM.Piet.Syntax
 
 import           Relude.Extra                               ( next, prev )
 
-move ∷ Eq a => Bounded a ⇒ Enum a ⇒ DirectionPointer → (a, a) → (a, a)
+move ∷ Eq a ⇒ Bounded a ⇒ Enum a ⇒ DirectionPointer → (a, a) → (a, a)
 move DPRight = first  next
 move DPDown  = second next
 move DPLeft  = first  prev
--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/Internal/ToRGB8.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/Internal/ToRGB8.hs
@@ -11,6 +11,8 @@ import           Control.Monad.Except
 import           Data.Bits
 import           Prelude              hiding ( fail )
 
+import           Relude.Extra         ( safeToEnum )
+
 toRGB8ImageM ∷ MonadError String m ⇒ DynamicImage → m (Image PixelRGB8)
 toRGB8ImageM (ImageY8     _)     = failConversion
 toRGB8ImageM (ImageY16    _)     = failConversion
@@ -38,9 +40,10 @@ class Pixel a => ToRGB8 a where
 instance ToRGB8 PixelRGB8 where
   toRGB8Pixel = id
 
+
 instance ToRGB8 PixelRGB16 where
   toRGB8Pixel (PixelRGB16 r g b) = PixelRGB8 (drop8 r) (drop8 g) (drop8 b) where
-    drop8 = toEnum . fromEnum . (`shiftR` 8)
+    drop8 x = fromMaybe 0 . safeToEnum . fromEnum $ shiftR x 8
 
 instance ToRGB8 PixelRGBF where
   toRGB8Pixel (PixelRGBF r g b) = PixelRGB8 (toI r) (toI g) (toI b) where
