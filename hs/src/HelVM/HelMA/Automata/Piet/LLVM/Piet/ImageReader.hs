978a382e38a447d5680f02288164f589615c077c
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-08-27 21:57:48 +0200

    Add LLVM

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/ImageReader.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/ImageReader.hs
@@ -13,7 +13,6 @@ module HelVM.HelMA.Automata.Piet.LLVM.Piet.ImageReader
 
 import           Codec.Picture
 import           Control.Monad.Except
-import           Data.Foldable                                          ( maximumBy )
 import qualified Data.Foldable1                                         as F1
 import qualified Data.List.NonEmpty                                     as NE
 import qualified Data.Map                                               as M
@@ -91,7 +90,9 @@ getCodelColor strategy codelSizeInt image codelX codelY = getCodelColor' strateg
   getCodelColor' MulticoloredCodelAsWhite = if hasMultipleColors then PixelRGB8 0xFF 0xFF 0xFF else firstColor
   getCodelColor' MulticoloredCodelAsBlack = if hasMultipleColors then PixelRGB8 0x00 0x00 0x00 else firstColor
   getCodelColor' MulticoloredCodelCenter = pixelAt image (pixelOffsetX + codelSizeInt `div` 2) (pixelOffsetY + codelSizeInt `div` 2)
-  getCodelColor' MulticoloredCodelModal = NE.head $ maximumBy (comparing length) $ NE.groupAllWith id colors
+  getCodelColor' MulticoloredCodelModal = case NE.nonEmpty (NE.groupAllWith id colors) of
+    Just grouped -> NE.head $ F1.maximumBy (comparing length) grouped
+    Nothing      -> firstColor
   getCodelColor' MulticoloredCodelAverage = average where
     average = PixelRGB8 (fromIntegral $ iR `div` codelsNum)
                         (fromIntegral $ iG `div` codelsNum)
@@ -101,9 +102,9 @@ getCodelColor strategy codelSizeInt image codelX codelY = getCodelColor' strateg
                          (0, 0, 0) colors
     codelsNum = toInteger $ codelSizeInt * codelSizeInt
   hasMultipleColors = any (/= firstColor) colors
-  firstColor = getHead colors
-  getHead (c : _) = c
-  getHead []      = error "unreachable"
+  firstColor = case colors of
+    (c : _) -> c
+    []      -> PixelRGB8 0 0 0
   colors = do
     pixelY <- [pixelOffsetY .. pixelOffsetY + codelSizeInt - 1]
     pixelX <- [pixelOffsetX .. pixelOffsetX + codelSizeInt - 1]
@@ -117,30 +118,31 @@ colorToCodel AdditionalColorAsBlack color = M.findWithDefault BlackCodel color c
 colorToCodel AdditionalColorNearest color = nearestCodel where
   squaredColorDistance (PixelRGB8 r1 g1 b1) (PixelRGB8 r2 g2 b2) = square r1 r2 + square g1 g2 + square b1 b2
   square a b = (toInteger a - toInteger b) ^ (2 :: Int)
-  nearestCodel = snd $ F1.minimum $ NE.fromList $ first (squaredColorDistance color) <$> colorCodelTableList
+  nearestCodel = snd $ F1.minimum $ first (squaredColorDistance color) <$> colorCodelTableList
 
 colorCodelTable ∷ M.Map PixelRGB8 Codel
-colorCodelTable = M.fromList colorCodelTableList
+colorCodelTable = M.fromList $ NE.toList colorCodelTableList
 
-colorCodelTableList ∷ [(PixelRGB8, Codel)]
-colorCodelTableList = [ (PixelRGB8 0xFF 0xC0 0xC0, AchromaticCodel Red Light)
-                      , (PixelRGB8 0xFF 0x00 0x00, AchromaticCodel Red Normal)
-                      , (PixelRGB8 0xC0 0x00 0x00, AchromaticCodel Red Dark)
-                      , (PixelRGB8 0xFF 0xFF 0xC0, AchromaticCodel Yellow Light)
-                      , (PixelRGB8 0xFF 0xFF 0x00, AchromaticCodel Yellow Normal)
-                      , (PixelRGB8 0xC0 0xC0 0x00, AchromaticCodel Yellow Dark)
-                      , (PixelRGB8 0xC0 0xFF 0xC0, AchromaticCodel Green Light)
-                      , (PixelRGB8 0x00 0xFF 0x00, AchromaticCodel Green Normal)
-                      , (PixelRGB8 0x00 0xC0 0x00, AchromaticCodel Green Dark)
-                      , (PixelRGB8 0xC0 0xFF 0xFF, AchromaticCodel Cyan Light)
-                      , (PixelRGB8 0x00 0xFF 0xFF, AchromaticCodel Cyan Normal)
-                      , (PixelRGB8 0x00 0xC0 0xC0, AchromaticCodel Cyan Dark)
-                      , (PixelRGB8 0xC0 0xC0 0xFF, AchromaticCodel Blue Light)
-                      , (PixelRGB8 0x00 0x00 0xFF, AchromaticCodel Blue Normal)
-                      , (PixelRGB8 0x00 0x00 0xC0, AchromaticCodel Blue Dark)
-                      , (PixelRGB8 0xFF 0xC0 0xFF, AchromaticCodel Magenta Light)
-                      , (PixelRGB8 0xFF 0x00 0xFF, AchromaticCodel Magenta Normal)
-                      , (PixelRGB8 0xC0 0x00 0xC0, AchromaticCodel Magenta Dark)
-                      , (PixelRGB8 0xFF 0xFF 0xFF, WhiteCodel)
-                      , (PixelRGB8 0x00 0x00 0x00, BlackCodel)
-                      ]
+colorCodelTableList ∷ NonEmpty (PixelRGB8, Codel)
+colorCodelTableList =
+  (PixelRGB8 0xFF 0xC0 0xC0, AchromaticCodel Red Light)
+  :| [ (PixelRGB8 0xFF 0x00 0x00, AchromaticCodel Red Normal)
+      , (PixelRGB8 0xC0 0x00 0x00, AchromaticCodel Red Dark)
+      , (PixelRGB8 0xFF 0xFF 0xC0, AchromaticCodel Yellow Light)
+      , (PixelRGB8 0xFF 0xFF 0x00, AchromaticCodel Yellow Normal)
+      , (PixelRGB8 0xC0 0xC0 0x00, AchromaticCodel Yellow Dark)
+      , (PixelRGB8 0xC0 0xFF 0xC0, AchromaticCodel Green Light)
+      , (PixelRGB8 0x00 0xFF 0x00, AchromaticCodel Green Normal)
+      , (PixelRGB8 0x00 0xC0 0x00, AchromaticCodel Green Dark)
+      , (PixelRGB8 0xC0 0xFF 0xFF, AchromaticCodel Cyan Light)
+      , (PixelRGB8 0x00 0xFF 0xFF, AchromaticCodel Cyan Normal)
+      , (PixelRGB8 0x00 0xC0 0xC0, AchromaticCodel Cyan Dark)
+      , (PixelRGB8 0xC0 0xC0 0xFF, AchromaticCodel Blue Light)
+      , (PixelRGB8 0x00 0x00 0xFF, AchromaticCodel Blue Normal)
+      , (PixelRGB8 0x00 0x00 0xC0, AchromaticCodel Blue Dark)
+      , (PixelRGB8 0xFF 0xC0 0xFF, AchromaticCodel Magenta Light)
+      , (PixelRGB8 0xFF 0x00 0xFF, AchromaticCodel Magenta Normal)
+      , (PixelRGB8 0xC0 0x00 0xC0, AchromaticCodel Magenta Dark)
+      , (PixelRGB8 0xFF 0xFF 0xFF, WhiteCodel)
+      , (PixelRGB8 0x00 0x00 0x00, BlackCodel)
+      ]
