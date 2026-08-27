969e8d1b09742c8c0e90331c6a9127a160a6cc81
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-08-27 20:17:39 +0200

    Add LLVM

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/Internal/Cyclic.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Piet/Internal/Cyclic.hs
@@ -1,7 +1,10 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use next" #-}
+
 module HelVM.HelMA.Automata.Piet.LLVM.Piet.Internal.Cyclic
   ( cyclicSucc
   ) where
 
 cyclicSucc ∷ (Eq a, Enum a, Bounded a) ⇒ a → a
 cyclicSucc x | x == maxBound = minBound
-             | otherwise = succ x
+             | otherwise     = succ x
