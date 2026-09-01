05956acdf47051146be362b23d6ac55a7db5f091
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-01 15:05:47 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Codel.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Codel.hs
@@ -2,11 +2,12 @@ module HelVM.HelMA.Automata.Piet.LLVM.Codel
   ( Codel (..)
   ) where
 
+import HelVM.HelMA.Automata.Piet.Types.ChromaticColor
 import           HelVM.HelMA.Automata.Piet.Types.Hue
 import           HelVM.HelMA.Automata.Piet.Types.Lightness
 
 data Codel
-  = AchromaticCodel Hue Lightness
+  = AchromaticCodel ChromaticColor
   | WhiteCodel
   | BlackCodel
   deriving stock (Eq, Ord, Show)
