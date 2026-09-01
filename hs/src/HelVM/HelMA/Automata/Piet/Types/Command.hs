4a5848dee4e48da0a28209fa47d5c20da2a3ec66
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-01 13:37:37 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/Types/Command.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/Types/Command.hs
@@ -1,16 +1,15 @@
 module HelVM.HelMA.Automata.Piet.Types.Command
   ( Command (..)
-  , commandConstructors
   , commandFromTransition
   , showCommand
   ) where
 
 import           HelVM.HelMA.Automata.Piet.LLVM.Codel
 
-
 import           Data.Vector                          ( Vector )
 import qualified Data.Vector.Generic                  as V
 
+-- TYPES
 
 data Command
   = NoOperation
@@ -33,32 +32,41 @@ data Command
   | OutChar
   deriving stock (Eq, Show)
 
-commandFromTransition ∷ (Hue, Lightness) → (Hue, Lightness) → Int → Command
-commandFromTransition (currentHue, currentLightness) (nextHue, nextLightness) = cmd where
-  cmd = commandConstructors V.! (hueDiff * 3 + lightnessDiff)
-  hueDiff = (fromEnum nextHue - fromEnum currentHue) `mod` 6
-  lightnessDiff = (fromEnum nextLightness - fromEnum currentLightness) `mod` 3
+-- FUNCTIONS
 
-commandConstructors ∷ Vector (Int → Command)
-commandConstructors = V.fromList [ const NoOperation
-                                 , Push
-                                 , const Pop
-                                 , const Add
-                                 , const Subtract
-                                 , const Multiply
-                                 , const Divide
-                                 , const Mod
-                                 , const Not
-                                 , const Greater
-                                 , const Pointer
-                                 , const Switch
-                                 , const Duplicate
-                                 , const Roll
-                                 , const InNumber
-                                 , const InChar
-                                 , const OutNumber
-                                 , const OutChar
-                                 ]
+commandFromTransition ∷ (Hue, Lightness) → (Hue, Lightness) → Int → Command
+commandFromTransition (currentHue, currentLightness) (nextHue, nextLightness) = commandConstructorsVector V.! ( hueDiff nextHue currentHue * 3 + lightnessDiff nextLightness currentLightness)
+
+hueDiff ∷ (Enum a1, Enum a2) ⇒ a1 → a2 → Int
+hueDiff nextHue currentHue = (fromEnum nextHue - fromEnum currentHue) `mod` 6
+
+lightnessDiff ∷ (Enum a1, Enum a2) ⇒ a1 → a2 → Int
+lightnessDiff nextLightness currentLightness = (fromEnum nextLightness - fromEnum currentLightness) `mod` 3
+
+commandConstructorsVector ∷ Vector (Int → Command)
+commandConstructorsVector = V.fromList commandConstructors
+
+commandConstructors ∷ [Int → Command]
+commandConstructors =
+  [ const NoOperation
+  , Push
+  , const Pop
+  , const Add
+  , const Subtract
+  , const Multiply
+  , const Divide
+  , const Mod
+  , const Not
+  , const Greater
+  , const Pointer
+  , const Switch
+  , const Duplicate
+  , const Roll
+  , const InNumber
+  , const InChar
+  , const OutNumber
+  , const OutChar
+  ]
 
 showCommand ∷ Command → String
 showCommand NoOperation = "noop"
