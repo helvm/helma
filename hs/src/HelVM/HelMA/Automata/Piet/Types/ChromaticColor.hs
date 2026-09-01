e9191e992e5e9a6d9faa02b153fd46b5c1f4184d
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-01 15:04:15 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/Types/ChromaticColor.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/Types/ChromaticColor.hs
@@ -11,8 +11,8 @@ chromaticDiffToIndex ∷ (Hue, Lightness) → (Hue, Lightness) → Int
 chromaticDiffToIndex (h1, l1) (h2, l2) = hueDiff h1 h2 * 3 + lightnessDiff l1 l2
 
 chromaticDiff ∷ ChromaticColor → ChromaticColor → ChromaticColor
-chromaticDiff (ChromaticColor l1 h1) (ChromaticColor l2 h2) = ChromaticColor (lightnessChange l1 l2) (hueChange h1 h2)
+chromaticDiff (ChromaticColor h1 l1) (ChromaticColor h2 l2) = ChromaticColor (hueChange h1 h2) (lightnessChange l1 l2)
 
 data ChromaticColor
-  = ChromaticColor !Lightness !Hue --FIXME revert parameters
+  = ChromaticColor !Hue !Lightness
   deriving stock (Eq, Ord, Read, Show)
