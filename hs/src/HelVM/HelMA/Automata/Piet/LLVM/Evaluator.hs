e0ba10402eff58f0f8d37d601c4dfee960c73433
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-03 10:58:56 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
@@ -8,7 +8,7 @@ import           HelVM.HelMA.Automata.Piet.LLVM.Parser
 import           HelVM.HelMA.Automata.Piet.SyntaxVisualizer
 import           HelVM.HelMA.Automata.Piet.Types.SyntaxGraph
 
-import           HelVM.HelIO.Control.Safe                    ( MonadSafe )
+import           HelVM.HelIO.Control.Safe
 import qualified HelVM.HelMA.Automaton.API.AppOptions        as App
 import           HelVM.HelMA.Automaton.API.Env
 
@@ -23,18 +23,22 @@ data PietStep
   | StepGenerateDOT
   deriving stock (Eq, Show)
 
-graphTextRio ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ (PietStep → RIO.RIO env ()) → ImageConfig → RIO.RIO env LText
-graphTextRio messageReceiver imageConfig = graphTextWithOptions =<< optionsRio where
-  graphTextWithOptions o = graphText messageReceiver imageConfig (App.file o)
+graphTextRio ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ ImageConfig → RIO.RIO env LText
+graphTextRio imageConfig = graphTextWithOptions =<< optionsRio where
+  graphTextWithOptions o = graphText imageConfig (App.file o)
 
-graphText ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ (PietStep → RIO.RIO env ()) → ImageConfig → FilePath → RIO.RIO env LText
-graphText messageReceiver imageConfig inputPath = do
-  messageReceiver StepGenerateDOT
-  syntaxToDOT <$> makeGraph messageReceiver imageConfig inputPath
+graphText ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ ImageConfig → FilePath → RIO.RIO env LText
+graphText imageConfig inputPath = do
+  logStep StepGenerateDOT
+  syntaxToDOT <$> makeGraph imageConfig inputPath
 
-makeGraph ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ (PietStep → RIO.RIO env ()) → ImageConfig → FilePath → RIO.RIO env (Maybe SyntaxGraph)
-makeGraph messageReceiver imageConfig inputPath = do
-  messageReceiver StepParse
-  image <- messageReceiver StepReadImage *> readImageRio inputPath
+makeGraph ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ ImageConfig → FilePath → RIO.RIO env (Maybe SyntaxGraph)
+makeGraph imageConfig inputPath = do
+  logStep StepParse
+  logStep StepReadImage
+  image <- readImageRio inputPath
   codels <- readCodels imageConfig image
   parse codels
+
+logStep ∷ Has env ⇒ PietStep → RIO.RIO env ()
+logStep step = RIO.logDebug (RIO.displayShow step)
