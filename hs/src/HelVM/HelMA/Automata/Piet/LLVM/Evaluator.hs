6193706bd9d03d6033bee5aa504195d3ff856b67
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-03 11:40:02 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
@@ -31,17 +31,20 @@ type MonadEvaluator m = (MonadIO m, MonadControl m)
 
 type MonadControl m = (MonadSafe m, MonadLogger m)
 
-evaluate ∷ MonadEvaluator m ⇒ (PietStep → m ()) → ImageConfig → FilePath → m LText
-evaluate messageReceiver imageConfig inputPath = graphText messageReceiver imageConfig =<< readImageFile inputPath
+evaluate ∷ MonadEvaluator m ⇒ ImageConfig → FilePath → m LText
+evaluate imageConfig inputPath = graphText imageConfig =<< readImageFile inputPath
+
+graphText ∷ MonadControl m ⇒ ImageConfig → DynamicImage → m LText
+graphText imageConfig image = logStep StepGenerateDOT *> (syntaxToDOT <$> makeGraph imageConfig image)
+
+makeGraph ∷ MonadControl m ⇒ ImageConfig → DynamicImage → m (Maybe SyntaxGraph)
+makeGraph imageConfig image = logStep StepParse *> (parse =<< readCodelsWithStep imageConfig image)
+
+readCodelsWithStep ∷ MonadControl m ⇒ ImageConfig → DynamicImage → m (Matrix Color)
+readCodelsWithStep imageConfig image = logStep StepReadImage *> readCodels imageConfig image
 
 readImageFile ∷ MonadEvaluator m ⇒ FilePath → m DynamicImage
 readImageFile filePath = liftEitherLegacy =<< liftIO (readImage filePath)
 
-graphText ∷ MonadControl m ⇒ (PietStep → m ()) → ImageConfig → DynamicImage → m LText
-graphText messageReceiver imageConfig image = messageReceiver StepGenerateDOT *> (syntaxToDOT <$> makeGraph messageReceiver imageConfig image)
-
-makeGraph ∷ MonadControl m ⇒ (PietStep → m ()) → ImageConfig → DynamicImage → m (Maybe SyntaxGraph)
-makeGraph messageReceiver imageConfig image = messageReceiver StepParse *> (parse =<< readCodelsWithStep messageReceiver imageConfig image)
-
-readCodelsWithStep ∷ MonadControl m ⇒ (PietStep → m ()) → ImageConfig → DynamicImage → m (Matrix Color)
-readCodelsWithStep messageReceiver imageConfig image = messageReceiver StepReadImage *> readCodels imageConfig image
+logStep ∷ MonadLogger m ⇒ PietStep → m ()
+logStep step = logDebugN $ show step
