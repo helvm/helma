e87f9bb03e4dfb34243c5f33f8adf78a85cba326
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-03 10:54:54 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
@@ -1,16 +1,18 @@
 module HelVM.HelMA.Automata.Piet.LLVM.Evaluator
   ( graphText
-  , nullReceiver
+  , graphTextRio
   ) where
-
-import           HelVM.HelIO.Control.Safe
 
 import           HelVM.HelMA.Automata.Piet.LLVM.ImageReader
 import           HelVM.HelMA.Automata.Piet.LLVM.Parser
 import           HelVM.HelMA.Automata.Piet.SyntaxVisualizer
 import           HelVM.HelMA.Automata.Piet.Types.SyntaxGraph
 
-import           Codec.Picture
+import           HelVM.HelIO.Control.Safe                    ( MonadSafe )
+import qualified HelVM.HelMA.Automaton.API.AppOptions        as App
+import           HelVM.HelMA.Automaton.API.Env
+
+import qualified RIO
 
 data PietStep
   = StepReadImage
@@ -21,17 +23,18 @@ data PietStep
   | StepGenerateDOT
   deriving stock (Eq, Show)
 
--- Constraint Type Aliases
-type MonadEvaluator m = (MonadIO m, MonadSafe m)
+graphTextRio ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ (PietStep → RIO.RIO env ()) → ImageConfig → RIO.RIO env LText
+graphTextRio messageReceiver imageConfig = graphTextWithOptions =<< optionsRio where
+  graphTextWithOptions o = graphText messageReceiver imageConfig (App.file o)
 
-graphText ∷ MonadEvaluator m ⇒ (PietStep → m ()) → ImageConfig → FilePath → m LText
-graphText messageReceiver imageConfig inputPath = messageReceiver StepGenerateDOT *> (syntaxToDOT <$> makeGraph messageReceiver imageConfig inputPath)
+graphText ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ (PietStep → RIO.RIO env ()) → ImageConfig → FilePath → RIO.RIO env LText
+graphText messageReceiver imageConfig inputPath = do
+  messageReceiver StepGenerateDOT
+  syntaxToDOT <$> makeGraph messageReceiver imageConfig inputPath
 
-makeGraph ∷ MonadEvaluator m ⇒ (PietStep → m ()) → ImageConfig → FilePath → m (Maybe SyntaxGraph)
-makeGraph messageReceiver imageConfig inputPath = messageReceiver StepParse *> (parse =<< messageReceiver StepReadImage *> (readCodels imageConfig =<< readImageFile inputPath))
-
-readImageFile ∷ MonadEvaluator m ⇒ FilePath → m DynamicImage
-readImageFile filePath = liftEitherLegacy =<< liftIO (readImage filePath)
-
-nullReceiver ∷ Monad m ⇒ PietStep → m ()
-nullReceiver _ = pass
+makeGraph ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ (PietStep → RIO.RIO env ()) → ImageConfig → FilePath → RIO.RIO env (Maybe SyntaxGraph)
+makeGraph messageReceiver imageConfig inputPath = do
+  messageReceiver StepParse
+  image <- messageReceiver StepReadImage *> readImageRio inputPath
+  codels <- readCodels imageConfig image
+  parse codels
