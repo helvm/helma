f45cfdec340bea04c0b72713e1ed10aac21ca250
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-03 11:02:47 +0200

    Remove lens

--- hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/LLVM/Evaluator.hs
@@ -8,9 +8,10 @@ import           HelVM.HelMA.Automata.Piet.LLVM.Parser
 import           HelVM.HelMA.Automata.Piet.SyntaxVisualizer
 import           HelVM.HelMA.Automata.Piet.Types.SyntaxGraph
 
-import           HelVM.HelIO.Control.Safe
 import qualified HelVM.HelMA.Automaton.API.AppOptions        as App
 import           HelVM.HelMA.Automaton.API.Env
+
+import           HelVM.HelMA.Automaton.Extra
 
 import qualified RIO
 
@@ -23,22 +24,22 @@ data PietStep
   | StepGenerateDOT
   deriving stock (Eq, Show)
 
-graphTextRio ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ ImageConfig → RIO.RIO env LText
+graphTextRio ∷ Has env ⇒ ImageConfig → RIO.RIO env LText
 graphTextRio imageConfig = graphTextWithOptions =<< optionsRio where
   graphTextWithOptions o = graphText imageConfig (App.file o)
 
-graphText ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ ImageConfig → FilePath → RIO.RIO env LText
+graphText ∷ Has env ⇒ ImageConfig → FilePath → RIO.RIO env LText
 graphText imageConfig inputPath = do
   logStep StepGenerateDOT
   syntaxToDOT <$> makeGraph imageConfig inputPath
 
-makeGraph ∷ (Has env, MonadSafe (RIO.RIO env)) ⇒ ImageConfig → FilePath → RIO.RIO env (Maybe SyntaxGraph)
+makeGraph ∷ Has env ⇒ ImageConfig → FilePath → RIO.RIO env (Maybe SyntaxGraph)
 makeGraph imageConfig inputPath = do
   logStep StepParse
   logStep StepReadImage
   image <- readImageRio inputPath
-  codels <- readCodels imageConfig image
-  parse codels
+  codels <- runAsRIO $ readCodels imageConfig image
+  runAsRIO $ parse codels
 
 logStep ∷ Has env ⇒ PietStep → RIO.RIO env ()
 logStep step = RIO.logDebug (RIO.displayShow step)
