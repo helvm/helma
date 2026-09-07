9161955f462bb3323f0502a47f0317614a80edb0
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-07 12:18:58 +0200

    Refactor piet

--- hs/src/HelVM/HelMA/Automata/Piet/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/Evaluator.hs
@@ -55,10 +55,10 @@ runWithOptions ∷ Has env ⇒ PietOptions → App.AppOptions → RIO.RIO env ()
 runWithOptions po o = run (App.emit o) po =<< readImageRio (App.file o)
 
 run ∷ Has env ⇒ Emit → PietOptions → DynamicImage → RIO.RIO env ()
-run No options = runAsRIO . simpleEval options.implType options.limit
-run IL po      =  putLTextLnRio <=< (runAsRIO . emitIL . imageInput po)
-run TL po      = putLTextLnRio <=< (runAsRIO . emitCommands . imageInput po)
-run _ po       = putLTextLnRio <=< (runAsRIO . emitDot . imageInput po)
+run No po = runAsRIO . simpleEval po.implType po.codelSize
+run IL po =  putLTextLnRio <=< (runAsRIO . emitIL . imageInput po)
+run TL po = putLTextLnRio <=< (runAsRIO . emitCommands . imageInput po)
+run _ po  = putLTextLnRio <=< (runAsRIO . emitDot . imageInput po)
 
 simpleEval ∷ AppSafeEff m ⇒ ImplType → Maybe CodelSize → DynamicImage → m ()
 simpleEval i cs = start i . uncurry compile <=< logCS . processImage cs
