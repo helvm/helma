d01c45636ec9997bec9eaeed93d7a2fe325602b5
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-07 11:19:22 +0200

    Refactor piet

--- hs/src/HelVM/HelMA/Automata/Piet/API/PietOptions.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/API/PietOptions.hs
@@ -1,1 +1,14 @@
 module HelVM.HelMA.Automata.Piet.API.PietOptions where
+
+import           HelVM.HelMA.Automata.Piet.API.AdditionalColorStrategy
+import           HelVM.HelMA.Automata.Piet.API.ImplType                 
+import           HelVM.HelMA.Automata.Piet.API.LexerType
+import           HelVM.HelMA.Automata.Piet.API.MulticoloredCodelStrategy
+
+data PietOptions = PietOptions
+  { implType                  :: !ImplType
+  , additionalColorStrategy   :: !(Maybe AdditionalColorStrategy)
+  , multicoloredCodelStrategy :: !(Maybe MulticoloredCodelStrategy)
+  , limit                     :: !(Maybe Natural)
+  , lexerType                 :: !(Maybe LexerType)
+  } deriving stock (Eq, Show)
