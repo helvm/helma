9061af0201f8aa9e1da8e5def23d9f74b4b4e499
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-08 18:07:55 +0200

    Add piet common

--- hs/src/HelVM/HelMA/Automata/Piet/Evaluator.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/Evaluator.hs
@@ -37,8 +37,7 @@ import qualified HelVM.HelMA.Automaton.API.AppOptions          as App
 import           HelVM.HelMA.Automaton.API.AutomatonType
 import           HelVM.HelMA.Automaton.API.Emit
 import           HelVM.HelMA.Automaton.API.Env
-import           HelVM.HelMA.Automaton.API.EvalOptions         ( EvalOptions, simpleEvalOptions )
-import qualified HelVM.HelMA.Automaton.API.EvalOptions         as EvalOptions
+import           HelVM.HelMA.Automaton.API.EvalOptions         ( EvalOptions, automatonOptions, parserOptions, simpleEvalOptions )
 import           HelVM.HelMA.Automaton.API.OptimizationLevel
 import           HelVM.HelMA.Automaton.API.ParserOptions
 
@@ -63,7 +62,7 @@ runWithOptions po o = run (App.emit o) (App.evalOptions o) po =<< readImageRio (
 
 run ∷ Has env ⇒ Emit → EvalOptions → Options → DynamicImage → RIO.RIO env ()
 run No eo po = runAsRIO . evalParams (fromMaybe Custom (automatonType po)) eo po
-run IL eo po = putLTextLnRio <=< (runAsRIO . emitIL (optLevel $ EvalOptions.parserOptions eo) . imageInput po)
+run IL eo po = putLTextLnRio <=< (runAsRIO . emitIL (optLevel $ parserOptions eo) . imageInput po)
 run TL _  po = putLTextLnRio <=< (runAsRIO . emitCommands . imageInput po)
 run _  _  po = putLTextLnRio <=< (runAsRIO . emitDot . imageInput po)
 
@@ -75,7 +74,7 @@ simpleEval ∷ AppSafeEff m ⇒ DynamicImage → m ()
 simpleEval = evalCommon simpleEvalOptions simplePietOptions
 
 evalCommon ∷ AppSafeEff m ⇒ EvalOptions → Options → DynamicImage → m ()
-evalCommon eo po = flip Automaton.start (EvalOptions.automatonOptions eo) <=< generateIL (optLevel $ EvalOptions.parserOptions eo) . imageInput po
+evalCommon eo po = flip Automaton.start (automatonOptions eo) <=< generateIL (optLevel $ parserOptions eo) . imageInput po
 
 evalCustom ∷ AppSafeEff m ⇒ ImplType → Maybe CodelSize → DynamicImage → m ()
 evalCustom i cs = start i . uncurry compile <=< logCS . processImage cs
