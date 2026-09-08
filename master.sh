c9ad2ca4b2394f1fb5b97fec3a7e8bcbe17894ea
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-08 21:22:11 +0200

    Add piet common

--- master.sh
+++ master.sh
@@ -8,5 +8,10 @@ mkdir -p ../helma/examples
 rsync -av \
   examples/ ../helma/examples/
 
+mkdir -p ../helma/.output
+rsync -av \
+  .output/ ../helma/.output/
+
+
 rsync -av \
   helma.cabal ../helma/helma.cabal
