90d29761acc1ec850ff30272e952f0fc998643fd
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-14 19:23:41 +0200

    Remove matrix

--- hs/test/HelVM/HelMA/Automata/Piet/SyntaxParserSpec.hs
+++ hs/test/HelVM/HelMA/Automata/Piet/SyntaxParserSpec.hs
@@ -28,7 +28,7 @@ import           Test.Hspec
 data ImageTestCase
   = ImageTestCase
       { caseName      :: String
-      , testImage     :: Matrix Codel
+      , testImage     :: Grid Codel
       , blockTable    :: IntMap BlockCoordinates
       , expectedGraph :: Maybe SyntaxGraph
       }
@@ -66,7 +66,7 @@ spec = do
       , ImageTestCase "complexImage" complexImage complexBlockTable expectedComplexGraph
       ] $ \tc ->
         context ("when given " ++ caseName tc) $ do
-          res <- runIO . runSafeT $ parseFilledGrid (matrixToGrid $ testImage tc, blockTable tc)
+          res <- runIO . runSafeT $ parseFilledGrid (testImage tc, blockTable tc)
           xit "returns a syntax graph" $ safeToEitherLegacy res `shouldBe` Right (expectedGraph tc)
 
     forM_
@@ -144,8 +144,8 @@ spec = do
           it ("returns " ++ show (command12 tc, command21 tc) ++ " when given " ++ show (color1 tc, color2 tc)) $ safeToEitherLegacy res `shouldBe` Right expectedG
 
 
-smallImage ∷ Matrix Codel
-smallImage = toVector2D [[Codel (Chromatic $ ChromaticColor Red Normal) 0]]
+smallImage ∷ Grid Codel
+smallImage = toGrid [[Codel (Chromatic $ ChromaticColor Red Normal) 0]]
 
 smallBlockTable ∷ IntMap BlockCoordinates
 smallBlockTable = IM.fromList [(0, [(0, 0)])]
@@ -153,8 +153,8 @@ smallBlockTable = IM.fromList [(0, [(0, 0)])]
 expectedSmallGraph ∷ Maybe SyntaxGraph
 expectedSmallGraph = Just $ SyntaxGraph (BlockEdge 0 rl) $ IM.fromList [(0, Block M.empty)]
 
-whiteImage ∷ Matrix Codel
-whiteImage = toVector2D [[Codel White 0]]
+whiteImage ∷ Grid Codel
+whiteImage = toGrid [[Codel White 0]]
 
 whiteBlockTable ∷ IntMap BlockCoordinates
 whiteBlockTable = IM.fromList [(0, [(0, 0)])]
@@ -165,8 +165,8 @@ blackImage = toVector2D [[Codel Black 0]]
 blackBlockTable ∷ IntMap BlockCoordinates
 blackBlockTable = IM.fromList [(0, [(0, 0)])]
 
-distantInitialImage ∷ Matrix Codel
-distantInitialImage = toVector2D
+distantInitialImage ∷ Grid Codel
+distantInitialImage = toGrid
   [ [ Codel White 0
     , Codel White 0
     , Codel White 0
@@ -200,8 +200,8 @@ expectedDistantInitialGraph = Just $ SyntaxGraph (BlockEdge 1 ur) $ IM.fromList
     )
   ]
 
-stuckImage ∷ Matrix Codel
-stuckImage = toVector2D
+stuckImage ∷ Grid Codel
+stuckImage = toGrid
   [ [ Codel (Chromatic $ ChromaticColor Red Light) 0
     , Codel (Chromatic $ ChromaticColor Red Normal) 1
     , Codel White 2
@@ -384,8 +384,8 @@ rawComplexImage = toGrid
     ]
   ]
 
-complexImage ∷ Matrix Codel
-complexImage = toVector2D
+complexImage ∷ Grid Codel
+complexImage = toGrid
   [ [ Codel (Chromatic $ ChromaticColor Blue Dark) 0
     , Codel (Chromatic $ ChromaticColor Blue Dark) 0
     , Codel (Chromatic $ ChromaticColor Blue Dark) 0
