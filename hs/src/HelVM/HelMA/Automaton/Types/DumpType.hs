82513d8b9e07c7e7c5ad23a50e36b07af237b6d7
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-15 18:04:02 +0200

    opt

--- hs/src/HelVM/HelMA/Automaton/Types/DumpType.hs
+++ hs/src/HelVM/HelMA/Automaton/Types/DumpType.hs
@@ -4,18 +4,25 @@ import           HelVM.HelMA.Automaton.Eff.MonadEff
 
 import           Control.Monad.Logger
 
-import           Text.Pretty.Simple
+import qualified Data.Text.Lazy                     as LT
+import qualified Data.Text.Lazy.Builder             as B
+import           Text.Pretty.Simple                 ( pShowNoColor )
 
-logDump ∷ (AppSafeEff m , Show d) ⇒ DumpType → d → m ()
-logDump dt d = flip whenJust logInfoNL $ dump dt d
+logDump ∷ (AppSafeEff m, Show d) ⇒ DumpType → d → m ()
+logDump No     _ = pass
+logDump Ugly   d = logInfoNL $ B.toLazyText $ B.fromString $ show d
+logDump Pretty d = logInfoNL $ pShowNoColor d
+{-# INLINE logDump #-}
 
-logInfoNL ∷ MonadLogger m ⇒ LText → m ()
-logInfoNL  = logWithoutLoc "" LevelInfo . toLogStr
+logInfoNL ∷ MonadLogger m ⇒ LT.Text → m ()
+logInfoNL = logWithoutLoc "" LevelInfo . toLogStr
+{-# INLINE logInfoNL #-}
 
-dump ∷ Show a ⇒ DumpType → a → Maybe LText
+dump ∷ Show a ⇒ DumpType → a → Maybe LT.Text
 dump No     _ = Nothing
-dump Ugly   a = Just $ show  a
+dump Ugly   a = Just $ B.toLazyText $ B.fromString $ show a
 dump Pretty a = Just $ pShowNoColor a
+{-# INLINABLE dump #-}
 
 -- | Constructors
 defaultDumpType ∷ DumpType
