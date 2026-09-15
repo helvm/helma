c814985523cafd793bba88bf37fd7dc67d519055
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-15 11:14:24 +0200

    Remove matrix

--- hs/src/HelVM/HelMA/Automata/Piet/Filler.hs
+++ hs/src/HelVM/HelMA/Automata/Piet/Filler.hs
@@ -24,24 +24,24 @@ fillST image (_, 0) = pure (V.map (const V.empty) image, IM.empty)
 fillST image dim    = formatResult image dim =<< buildState image dim
 
 buildState ∷ Eq a ⇒ Matrix a → Coordinates → ST s (UMV.MVector s Int, IntMap BlockCoordinates)
-buildState image (h, w) = UMV.replicate (h * w) (-1) >>= \refs -> (refs,) <$> scanGrid image h w refs 0 0 0 IM.empty
+buildState image dim@(h, w) = UMV.replicate (h * w) (-1) >>= \refs -> (refs,) <$> scanGrid image dim refs 0 0 0 IM.empty
 
-scanGrid ∷ Eq a ⇒ Matrix a → Int → Int → UMV.MVector s Int → Int → Int → Int → IntMap BlockCoordinates → ST s (IntMap BlockCoordinates)
-scanGrid _ h _ _ y _ _ accMap | y >= h = pure accMap
-scanGrid image h w refs y x blockId accMap
-  | x >= rowLen y = scanGrid image h w refs (y + 1) 0 blockId accMap
+scanGrid ∷ Eq a ⇒ Matrix a → Coordinates → UMV.MVector s Int → Int → Int → Int → IntMap BlockCoordinates → ST s (IntMap BlockCoordinates)
+scanGrid _ (h, _) _ y _ _ accMap | y >= h = pure accMap
+scanGrid image (h, w) refs y x blockId accMap
+  | x >= rowLen y = scanGrid image (h, w) refs (y + 1) 0 blockId accMap
   | otherwise     = checkCell image h w refs y x blockId accMap =<< UMV.unsafeRead refs (y * w + x)
   where
     rowLen = V.length . (image V.!)
 
 checkCell ∷ Eq a ⇒ Matrix a → Int → Int → UMV.MVector s Int → Int → Int → Int → IntMap BlockCoordinates → Int → ST s (IntMap BlockCoordinates)
-checkCell image h w refs y x blockId accMap (-1) = maybe (scanGrid image h w refs y (x + 1) blockId accMap) (runFill image h w refs y x blockId accMap) (getPixel image x y)
-checkCell image h w refs y x blockId accMap _    = scanGrid image h w refs y (x + 1) blockId accMap
+checkCell image h w refs y x blockId accMap (-1) = maybe (scanGrid image (h, w) refs y (x + 1) blockId accMap) (runFill image h w refs y x blockId accMap) (getPixel image x y)
+checkCell image h w refs y x blockId accMap _    = scanGrid image (h, w) refs y (x + 1) blockId accMap
 
 runFill ∷ Eq a ⇒ Matrix a → Int → Int → UMV.MVector s Int → Int → Int → Int → IntMap BlockCoordinates → a → ST s (IntMap BlockCoordinates)
 runFill image h w refs y x blockId accMap targetCol =
   processBlock image h w refs targetCol blockId [(x, y)] [] >>= \coords ->
-    scanGrid image h w refs y (x + 1) (blockId + 1) (IM.insert blockId coords accMap)
+    scanGrid image (h, w) refs y (x + 1) (blockId + 1) (IM.insert blockId coords accMap)
 
 processBlock ∷ Eq a ⇒ Matrix a → Int → Int → UMV.MVector s Int → a → Int → BlockCoordinates → BlockCoordinates → ST s BlockCoordinates
 processBlock _ _ _ _ _ _ [] acc = pure acc
@@ -59,9 +59,8 @@ validNeighbors ∷ Eq a ⇒ Matrix a → Int → Coordinates → a → BlockCoor
 validNeighbors image h (x, y) targetCol = filter (isTarget image h targetCol) [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 isTarget ∷ Eq a ⇒ Matrix a → Int → a → Coordinates → Bool
-isTarget image h targetCol (nx, ny) = ny >= 0 && ny < h && nx >= 0 && nx < rowLen ny && getPixel image nx ny == Just targetCol
-  where
-    rowLen y = V.length (image V.! y)
+isTarget image h targetCol (nx, ny) = ny >= 0 && ny < h && nx >= 0 && nx < rowLen ny && getPixel image nx ny == Just targetCol where
+  rowLen y = V.length (image V.! y)
 
 formatResult ∷ Matrix a → Coordinates → (UMV.MVector s Int, IntMap BlockCoordinates) → ST s (Matrix Int, IntMap BlockCoordinates)
 formatResult image p (refs, blockMap) = (, blockMap) <$> formatResult' image p refs
