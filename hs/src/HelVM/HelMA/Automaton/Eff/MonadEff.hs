bef912c40916a3ab220fc158f618f95f21202a74
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-20 16:10:50 +0200

    opt

--- hs/src/HelVM/HelMA/Automaton/Eff/MonadEff.hs
+++ hs/src/HelVM/HelMA/Automaton/Eff/MonadEff.hs
@@ -17,53 +17,94 @@ type AppSafeEff m = (MonadSafe m , AppEff m)
 
 type AppEff m = (MonadLogger m, MonadEff m)
 
-class Monad m => MonadEff m where
+class Monad m ⇒ MonadEff m where
 
-  putAsChar       :: Integral v ⇒ v → m ()
-  putAsDec        :: Integral v ⇒ v → m ()
-  getCharAs       :: Integral v ⇒ m v
-  getDecAs        :: Integral v ⇒ m v
+  putAsChar       ∷ Integral v ⇒ v → m ()
+  putAsDec        ∷ Integral v ⇒ v → m ()
+  getCharAs       ∷ Integral v ⇒ m v
+  getDecAs        ∷ Integral v ⇒ m v
 
-  putIntAsChar    :: Int → m ()
-  putIntAsDec     :: Int → m ()
-  getCharAsInt    :: m Int
-  getDecAsInt     :: m Int
+  putIntAsChar    ∷ Int → m ()
+  putIntAsDec     ∷ Int → m ()
+  getCharAsInt    ∷ m Int
+  getDecAsInt     ∷ m Int
 
-  getContentsBS   :: m LByteString
-  getContentsText :: m LText
-  getChar         :: m Char
-  getChars        :: m Text
-  putChar         :: Char → m ()
-  putChars        :: Text → m ()
+  getContentsBS   ∷ m LByteString
+  getContentsText ∷ m LText
+  getChar         ∷ m Char
+  getChars        ∷ m Text
+  putChar         ∷ Char → m ()
+  putChars        ∷ Text → m ()
 
-  flush           :: m ()
+  flush           ∷ m ()
 
   putAsChar      = putIntAsChar . fromIntegral
+  {-# INLINE putAsChar #-}
+
   putAsDec       = putIntAsDec  . fromIntegral
+  {-# INLINE putAsDec #-}
+
   getCharAs      = fromIntegral <$> getCharAsInt
+  {-# INLINE getCharAs #-}
+
   getDecAs       = fromIntegral <$> getDecAsInt
+  {-# INLINE getDecAs #-}
 
   putIntAsChar   = putChar . chr
+  {-# INLINE putIntAsChar #-}
+
   putIntAsDec    = putChars . show
+  {-# INLINE putIntAsDec #-}
+
   getCharAsInt   = ord <$> getChar
+  {-# INLINE getCharAsInt #-}
+
   getDecAsInt    = readTextUnsafe <$> getChars
+  {-# INLINE getDecAsInt #-}
 
   flush          = pass
+  {-# INLINE flush #-}
 
 instance {-# OVERLAPPABLE #-} (MonadTrans t, Monad m, MonadEff m) ⇒ MonadEff (t m) where
   getContentsBS   = lift getContentsBS
+  {-# INLINE getContentsBS #-}
+
   getContentsText = lift getContentsText
+  {-# INLINE getContentsText #-}
+
   getChar         = lift getChar
+  {-# INLINE getChar #-}
+
   getChars        = lift getChars
+  {-# INLINE getChars #-}
+
   putChar         = lift . putChar
+  {-# INLINE putChar #-}
+
   putChars        = lift . putChars
+  {-# INLINE putChars #-}
+
   flush           = lift flush
+  {-# INLINE flush #-}
 
 instance {-# OVERLAPPING #-} MonadEff (RIO.RIO Env) where
   getContentsBS   = getContentsBSRio
+  {-# INLINE getContentsBS #-}
+
   getContentsText = getContentsTextRio
+  {-# INLINE getContentsText #-}
+
   getChar         = getCharRio
+  {-# INLINE getChar #-}
+
   getChars        = getCharsRio
+  {-# INLINE getChars #-}
+
   putChar         = putCharRio
+  {-# INLINE putChar #-}
+
   putChars        = putCharsRio
+  {-# INLINE putChars #-}
+
   flush           = pass
+  {-# INLINE flush #-}
--- hs/src/HelVM/HelMA/Automaton/Extra.hs
+++ hs/src/HelVM/HelMA/Automaton/Extra.hs
@@ -6,7 +6,6 @@ import           HelVM.HelMA.Automaton.API.EvalParams
 import           HelVM.HelMA.Automaton.API.IOTypes
 
 import           HelVM.HelIO.Control.Message
-
 import           HelVM.HelIO.Control.Safe
 
 import           Control.Monad.Logger
@@ -17,8 +16,18 @@ runAsRIO ∷ (MonadIO m, MonadReader env m, Has env) ⇒ LoggingT (SafeT m) a �
 runAsRIO action = either (const RIO.exitFailure) pure =<< runAsRIOResult action
 
 runAsRIOResult ∷ (MonadIO m, MonadReader env m, Has env) ⇒ LoggingT (SafeT m) a → m (Either Messages a)
-runAsRIOResult action = go =<< runExceptT . runLoggingT action . logOutput =<< RIO.view RIO.logFuncL where
-  go res = res <$ whenLeft_ res logSafeError
+runAsRIOResult action = do
+  logFunc <- RIO.view RIO.logFuncL
+  runAndHandleError $ runLoggingT action (logOutput logFunc)
+
+runAsRIOResult' ∷ (MonadIO m, MonadReader env m, Has env) ⇒ NoLoggingT (SafeT m) a → m (Either Messages a)
+runAsRIOResult' action = runAndHandleError $ runNoLoggingT action
+
+runAndHandleError ∷ (MonadIO m, MonadReader env m, Has env) ⇒ SafeT m a → m (Either Messages a)
+runAndHandleError action = do
+  res <- runExceptT action
+  whenLeft_ res logSafeError
+  pure res
 
 logOutput ∷ (MonadIO m, RIO.HasLogFunc env) ⇒ env → p → RIO.LogSource → LogLevel → LogStr → m ()
 logOutput logFunc _ source level msg = RIO.runRIO logFunc $ RIO.logGeneric source (toRioLevel level) (RIO.displayBytesUtf8 $ fromLogStr msg)
