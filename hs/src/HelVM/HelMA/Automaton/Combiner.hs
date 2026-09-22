e8ad6a1e3b2c32556fa8748dbbb52c4b0abc8fbd
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-09-22 10:16:01 +0200

    opt

--- hs/src/HelVM/HelMA/Automaton/Combiner.hs
+++ hs/src/HelVM/HelMA/Automaton/Combiner.hs
@@ -80,8 +80,8 @@ toLSM (Memory _ s r) = LSM s r
 
 -- | Types
 
-type SF s r m = Memory s r → m $ MemorySame s r
--- type SF s r m = Memory s r → SameT m (Memory s r)
+-- type SF s r m = Memory s r → m $ MemorySame s r
+type SF s r m = Memory s r → SameT m (Memory s r)
 
 type F s r m = Memory s r → m $ Memory s r
 
